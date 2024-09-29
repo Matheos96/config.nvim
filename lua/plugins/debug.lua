d6bfaffb0cbe506c62f2e4cb1716bade38767674
Matheos Mattsson <matheos.mattsson@gmail.com>
2024-09-29 16:17:02 +0300

    added netcoredbg and python daps

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,4 +1,4 @@
----@diagnostic disable: missing-fields
+---@diagnostic disable: missing-fields, undefined-field
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
@@ -14,11 +14,11 @@ return {
     local dapui = require 'dapui'
     return {
       -- Basic debugging (Visual Studio style)
-      { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
-      { '<F11>', dap.step_into, desc = 'Debug: Step Into' },
-      { '<F10>', dap.step_over, desc = 'Debug: Step Over' },
-      { '<S-F11>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<F9>', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+      { '<F5>',    dap.continue,          desc = 'Debug: Start/Continue' },
+      { '<F11>',   dap.step_into,         desc = 'Debug: Step Into' },
+      { '<F10>',   dap.step_over,         desc = 'Debug: Step Over' },
+      { '<S-F11>', dap.step_out,          desc = 'Debug: Step Out' },
+      { '<F9>',    dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
       {
         '<leader>B',
         function()
@@ -34,11 +34,10 @@ return {
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
-
     require('mason-nvim-dap').setup {
       automatic_installation = true,
       handlers = {},
-      ensure_installed = {},
+      ensure_installed = { 'coreclr', 'python' },
     }
 
     dapui.setup {
@@ -61,5 +60,28 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    -- .NET debugging
+    local netcoredbg_path = vim.fn.stdpath('data') .. '/mason/packages/netcoredbg/netcoredbg/netcoredbg'
+    dap.adapters.coreclr = {
+      type = 'executable',
+      command = netcoredbg_path,
+      args = { '--interpreter=vscode' },
+    }
+
+    dap.configurations.cs = {
+      {
+        type = 'coreclr',
+        name = 'launch - netcoredbg',
+        request = 'launch',
+        console = 'integratedTerminal',
+        program = function()
+          os.execute('dotnet build -c Debug')
+          local dir = vim.loop.cwd() .. '/' .. vim.fn.glob 'bin/Debug/net*/'
+          local name = dir .. vim.fn.glob('*.csproj'):gsub('%.csproj$', '.dll')
+          return name
+        end,
+      },
+    }
   end,
 }
